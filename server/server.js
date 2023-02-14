891073a61821ac3a5fc5443def2b01000c3e1cc5
Jason Stewart <jason@jasonstewart.io>
2023-02-14 16:03:22 +0000

    restructure into client/server model

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 const generatorRouter = require("./routes/generator.router");
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("short"));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use("/generator", generatorRouter);
-
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname, "..", "build", "index.html");
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
