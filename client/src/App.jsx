406d39bc3e8f7682b1cd70229ba2521357c8fb6d
Jason Stewart <jason@jasonstewart.io>
2023-02-14 16:26:39 +0000

    begin client side render logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState(0);
+
+  async function getImage() {
+    return await fetch("http://localhost:3000/generator/1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        homeScore: 5,
+        awayScore: 3,
+      }),
+    });
+  }
+
+  useEffect(() => {
+    getImage()
+      .then((res) => res.blob())
+      .then((blob) => setImage(URL.createObjectURL(blob)));
+  }, []);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={image} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 const generatorRouter = require("./routes/generator.router");
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://127.0.0.1:5173" }));
 app.use(morgan("short"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
