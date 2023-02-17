5dd109d626a817221cd0cfc392894d1ce99578a9
Jason Stewart <jason@jasonstewart.io>
2023-02-17 08:23:19 +0000

    styling changes

--- client/src/components/FinalScore/FinalScore.jsx
+++ client/src/components/FinalScore/FinalScore.jsx
@@ -1,12 +1,38 @@
 import { useEffect, useState } from "react";
 import useFetchImage from "../../hooks/useFetchImage";
-import { Input, Flex, Button, VStack, FormControl, FormLabel, HStack, Spinner } from "@chakra-ui/react";
+import { Input, Flex, Button, VStack, FormControl, FormLabel, HStack, Select } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import WorkArea from "../UI/WorkArea";
 import Title from "../UI/Title";
 import FormSection from "../forms/FormSection";
 import ImageSection from "../UI/ImageSection";
 import { saveAs } from "file-saver";
+
+const options = [
+  { value: "Ashford-Town-(Middx)", label: "Ashford Town (Middlesex)" },
+  { value: "Basingstoke-Town", label: "Basingstoke Town" },
+  { value: "Bedfont-Sports", label: "Bedfont Sports" },
+  { value: "Binfield", label: "Binfield" },
+  { value: "Chertsey-Town", label: "Chertsey Town" },
+  { value: "Chipstead", label: "Chipstead" },
+  { value: "Guernsey", label: "Guernsey" },
+  { value: "Hanworth-Villa", label: "Hanworth Villa" },
+  { value: "Leatherhead", label: "Leatherhead" },
+  { value: "Marlow", label: "Marlow" },
+  { value: "Merstham", label: "Merstham" },
+  { value: "Northwood-FC", label: "Northwood FC" },
+  { value: "Southall", label: "Southall" },
+  { value: "South-Park-(Reigate)", label: "South Park (Reigate)" },
+  { value: "Sutton-Common-Rovers", label: "Sutton Common Rovers" },
+  { value: "Thatcham-Town", label: "Thatcham Town" },
+  {
+    value: "Tooting-&-Mitcham-United",
+    label: "Tooting & Mitcham United",
+  },
+  { value: "Uxbridge", label: "Uxbridge" },
+  { value: "Walton-&-Hersham", label: "Walton & Hersham" },
+  { value: "Westfield", label: "Westfield" },
+];
 
 const FinalScore = (props) => {
   const [imageRequest, setImageRequest] = useState({
@@ -18,11 +44,11 @@ const FinalScore = (props) => {
 
   const { data, loading, error } = useFetchImage("http://localhost:3000/generator/1", imageRequest);
 
-  useEffect(() => {
-    setImageRequest((previousRequest) => {
-      return { ...previousRequest };
-    });
-  }, []);
+  // useEffect(() => {
+  //   setImageRequest((previousRequest) => {
+  //     return { ...previousRequest };
+  //   });
+  // }, []);
 
   const formik = useFormik({
     initialValues: {
@@ -71,6 +97,27 @@ const FinalScore = (props) => {
                   onChange={formik.handleChange}
                 />
               </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="homeTeam">Home Badge</FormLabel>{" "}
+                <Select id="homeTeam" name="homeTeam" value={formik.values.homeTeam} onChange={formik.handleChange}>
+                  {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="awayTeam">Away Badge</FormLabel>
+                <Select id="awayTeam" name="awayTeam" value={formik.values.awayTeam} onChange={formik.handleChange}>
+                  {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+
               <HStack>
                 <Button isLoading={loading} type="submit">
                   Update
